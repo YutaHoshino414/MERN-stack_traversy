8d90d12676baab13593dc1269376fd0f8379eb74
USER <y.hoshino25@gmail.com>
2022-06-20 20:24:01 +0900

    Fetch project data from GQL-query

---
+++ client/src/components/ProjectCard.jsx
@@ -0,0 +1,18 @@
+const ProjectCard = ({project}) => {
+    return ( 
+        <div className="col-md-4 mt-5">
+            <div className="card">
+                <div className="card-body">
+                    <div className="d-flex justify-content-between align-items-center">
+                        <h5 className="card-title">{project.name}</h5>
+                        <a href={`/projects/${project.id}`} className="btn btn-primary">view</a>
+                    </div>
+                    <p className="small">Status: <b>{project.status}</b></p>
+                </div>
+            </div>
+
+        </div>
+     );
+}
+ 
+export default ProjectCard;
